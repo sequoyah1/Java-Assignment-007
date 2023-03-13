456c2374eaa49f1a51e7cf5c7a744c54414f9c1d
sequoyah1 <sequoyahschaefer@yahoo.com>
2023-03-13 01:49:37 -0400

    Sequoyah Schaefer Assignment 007

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,7 +1,27 @@
+/**
+ * Author Sequoyah Schaefer
+ * Since 3/13/2023
+ * Version 1.0
+ */
+
 import java.util.Scanner;
 
 public class MyMath {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        while (num1 != num2) {
+            {
+                if(num1 > num2)
+                    num1 = num1 - num2;
+                else
+                    num2 = num2 - num1;
+
+            }
+
+            System.out.println(num2);
     }
+}
 }
